e620b3e39603b3970acefb861235a4c8cbfb24a6
eshulman <eladking22@gmail.com>
2018-10-26 20:58:26 +0300

    with url gen

--- notifier.py
+++ notifier.py
@@ -1,50 +1,62 @@
 import inotify.adapters
 import boto3
 
-access_key = 'VC3KPVLT00J2M80Z1Y9G'
-secret_key = 'O0Y9640BeCMuCwNEU6GsMKirgHXUAiGsDw4RqzuF'
-rgw="http://192.168.14.26:8000"
+access_key = 'K786GVIA52ASAYHT7FB8'
+secret_key = 'vDQNf8WKxTClF1Ail8bSmMxMDys5PThO733lBGg5'
+rgw = 'http://192.168.1.16:8000'
+bucket = 'test1'
+notify_path = '/tmp'
 
 fileIndex = {}
 
 # The sequence of calls that new file in nfs is making.
-sequence = "IN_CREATE,IN_ATTRIB,IN_ATTRIB,IN_MODIFY,IN_CLOSE_WRITE"
+sequence = 'IN_CREATE,IN_ATTRIB,IN_ATTRIB,IN_MODIFY,IN_CLOSE_WRITE'
+
 
 def CheckDirectory():
-    i = inotify.adapters.Inotify()
-    
+    notifier = inotify.adapters.Inotify()
+
     # which folder to watch
-    i.add_watch('/nfs')
-    
-    for event in i.event_gen(yield_nones=False):
+    notifier.add_watch(notify_path)
+
+    for event in notifier.event_gen(yield_nones=False):
         (_, type_names, path, filename) = event
-        
-        print("File: " + filename + " Type: " + type_names[0])
-        
-        pathFile = path + "/" + filename
-        
+
+        print('File: ' + filename + ' Type: ' + type_names[0])
+
+        pathFile = path + '/' + filename
+
         # TODO: strictly monitor after the the types and not letting it reach 10 type call before
         #      deletion. Check if order is correspond to the the sequence and not just throw it into the array
-        if(fileIndex.has_key(pathFile) == False):
+        if pathFile not in fileIndex:
             fileIndex[pathFile] = []
             fileIndex[pathFile].append(type_names[0])
         else:
             fileIndex[pathFile].append(type_names[0])
-        
-        if(len(fileIndex[pathFile]) > 10):
-                print("Deleted key")
-                fileIndex.pop(pathFile,None)
-                
-        elif(sequence in ",".join(fileIndex[pathFile])):
-            print("Upload")
-            uploadToObject(path,filename)
-            fileIndex.pop(pathFile,None)
+
+        if (len(fileIndex[pathFile]) > 10):
+            print('Deleted key')
+            fileIndex.pop(pathFile, None)
+
+        elif (sequence in ','.join(fileIndex[pathFile])):
+            print('Upload')
+            uploadToObject(path, filename)
+            genPulicUrl(bucket, filename)
+            fileIndex.pop(pathFile, None)
             print(fileIndex)
-            
-def uploadToObject(path,filename):
-    
-    s3 = boto3.client('s3',endpoint_url=rgw,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
-    s3.upload_file(path + "/" + filename, "my-buc",filename)
+
+
+def uploadToObject(path, filename):
+    s3 = boto3.client('s3', endpoint_url=rgw, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    s3.upload_file(path + '/' + filename, bucket, filename)
+
+
+def genPulicUrl(bucket, filename):
+    s3 = boto3.client('s3', endpoint_url=rgw, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    s3.put_object_acl(Key=filename, Bucket=bucket, ACL='public-read')
+    print(rgw + '/' + bucket + '/' + filename)
+    return rgw + '/' + bucket + '/' + filename
+
 
 if __name__ == '__main__':
     CheckDirectory()
